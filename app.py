bf74548101a0673d3220001e30173bd0394eaeb1
CyborneCipher <cybornecipher@gmail.com>
2023-04-01 17:27:16 +0530

    sync up

--- app.py
+++ app.py
@@ -1,115 +1,210 @@
 from flask import Flask, url_for, request, redirect, jsonify
+from markupsafe import escape
 from datetime import datetime
 from pymongo import MongoClient
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS
+import secrets
 
-# loading templates
+# loading routes
+# import routes
 
 
 
-app = Flask(__name__) # referencing this file
-app.secret_key = "authanticatorHashPasswordSecretKey"
+app = Flask(__name__, template_folder="templates") # referencing this file
+cors = CORS(app, resources={
+	r"/api/*": {
+	"origins": "*"
+	}
+	})
+app.secret_key = "8ut4a6t1c2tor4a5hPa55word53cr6tK2y"
+
 
 
 try:
-	mongo_client = MongoClient('localhost', 27017)  # or app.config['MONGO_URI'] = 'mongodb://localhost:27017/authanticator_db'
+	mongo_client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=10000)  # or app.config['MONGO_URI'] = 'mongodb://localhost:27017/authanticator_db'
+	if(mongo_client.server_info()):
+		database = mongo_client['DatabaseAuthanticator']		# assigning the database to client
+		print('\n\n---------- Connection Successful ---------------\n\n')
+	else:
+		"""
+		source: https://kb.objectrocket.com/mongo-db/check-if-a-mongodb-server-is-running-using-the-pymongo-python-driver-643
+		MongoDB is a powerful document database that focuses on search and document retrieval capabilities. Exception raising is not the highest priority. But that should be okay giving what MongoDB has to offer far outweighs how it handles exceptions.
 
-	database = mongo_client['DatabaseAuthanticator']		# assigning the database to client
+		When the MongoClient() receives the settings of the port and domain and they aren’t the same as the server, an exception won’t automatically be raised. The good news is that there are ways to go around it so your coding isn’t hindered in the least.
+		
+		# Raise an Exception with server_info() client object method
+		-> The server_info() method of the client object can be called when MongoDB returns a client instance. You’re verifying that the instance is genuine. An exception ServerSelectionTimeoutError is shown when the port string and domain settings are wrong.
 
-	print('\n\n---------- Connection Successfull ---------------\n\n')
-except:
-	print('\n\nCould not connect to database. Error\n\n')
+		"""
+		raise Exception
+except Exception as e:
+	print('\n\nCould not connect to database. Error: ', e)
 
 
 # index route
 @app.route('/')
 def server():
+	"""
+	To access the qyery params, use request.args.get('name')
+	"""
 	return "Hello Pythonist! Your server is running ..."
+
 
 @app.route('/api/auth/signup', methods=['POST'])
 def signupRoute():
 	print('\n\nRequest Received >>> ', request.json)
-	_username = request.json['username'].lower()
+
+	# variable keys to save in the database
+	_username = request.json['email'].split('@')[0].lower()
 	_password = request.json['password']
 	_email = request.json['email'].lower()
 	_name = request.json['name']
 
-	collection = database['users'] 		# assigning the collection to database
+	collection = database['users'] 		# collection[table] being used
 
-	if request.method == 'POST':
-		_hashed_password = generate_password_hash(_password)
+	_hashed_password = generate_password_hash(_password)
 
-		try:
-			query = {'username': _username}
-			document = collection.find_one(query)
-			# make an api for email duplicate verification
-			if document and document['username'] == _username:
-				response = jsonify({
-					'message': 'User already signed up. Please try logging in!',
-					'give_access': False,
-					'creds_match': True
+	try:
+		# query to find the user with same email in document
+		query = {'email': _email}
+		document = collection.find_one(query)
+
+		# match the email with database for returning user
+		if document and document['email'] == _email:
+			response = jsonify({
+				'message': 'User already signed up. Please try logging in!',
+				'give_access': False
 				})
-				response.status_code == 200
-			else:
-				collection.insert_one({
-				    'name': _name,
-				    'email': _email,
-		            'username': _username,
-		            'password': _hashed_password,
-		            'last_updated': datetime.utcnow()
+			response.status_code == 200
+		else:
+			collection.insert_one({
+				'name': _name,
+				'email': _email,
+				'username': _username,
+				'password': _hashed_password,
+				'last_updated': datetime.utcnow(),
+				'secret_key': secrets.token_hex()
 				})
-				response = jsonify({
-					'message': "User added Successfully"
+			response = jsonify({
+				'message': "User added Successfully",
+				'give_access': True
 				})
-				response.status_code = 200
-		except:
-			print('Could not insert new user', e)
-			response = jsonify({'message': 'Could not signup a new user'})
-			response.status_code = 500
-		return response
-	else:
-		return not_found()
+			response.status_code = 200
+	except Exception as e:
+		print('\n\n\nSuffered Internal Server Error: ', e)
+		response = jsonify({
+			'message': 'Could not signup a new user',
+			'server_error': True,
+			'give_access': False
+			})
+		response.status_code = 500
+	return response
+# Signup Route Ends
 
 
 @app.route('/api/auth/login', methods=['POST'])
 def loginRoute():
 	print('\n\nRequest Recieved >>> ', request.json)
 
-	_username = request.json['username'].lower()
+	_email = request.json['email'].lower()
 	_password = request.json['password']
 
-	collection = database['users']
+	collection = database['users']	# collection[table] being used
 
-	if request.method == 'POST' and isinstance(_username, str) and isinstance(_password, str):
-		try:
-			query = {'username': _username}
-			document = collection.find(query)
+	try:
+		query = {'email': _email}
+		document = collection.find(query)
+
+		if collection.count_documents(query) == 1:
 			if check_password_hash(document[0]['password'], _password):
-				response = jsonify({
-					'message': 'Welcome returning user',
-					'give_access': True,
-					'creds_match': True
-               })
-				response.status_code = 200
+				data = []
+				for doc in document:
+					doc['_id'] = str(doc['_id'])
+					data.append(doc)
+					response = jsonify({
+						'message': 'Welcome returning user',
+						'give_access': True,
+						'data': data
+					})
+					response.status_code = 200
 			else:
 				response = jsonify({
 					'message': "Wrong Credentials. Please try again!",
-					'give_access': False,
-					'creds_match': False
-				})
-		except:
-			print('Could not find a collection with requested name ')
+					'give_access': False
+				}), 200
+		else:
+			print('Multiple Users found. ')
 			response = jsonify({
-				'message': 'Could not find the returning user. ',
+				'message': 'Server Error. Multiple Users found.',
 				'give_access': False,
-				'creds_match': None
-           })
-			response.status_code = 200
-		return response
-	else:
-		return not_found()
+			}), 500
+	except Exception as e:
+		print('\n\n\nSuffered Internal Server Error: ', e)
+		response = jsonify({
+			'message': 'Internal Server Error. ',
+			'give_access': False,
+			'server_error': True
+			}), 500
+	return response
+# Login Route Ends
+
+
+
+@app.route('/api/user/creds/update/<string:user_id>', methods=['PUT'])
+def updateProfile(user_id: str):
+	print('\n\n Updating Profile: ', user_id)
+	print('\n\n Request Received: ', request.json)
+
+	expected_keys = ['username', 'email', 'password', 'theme_mode', 'profile_image']
+
+	received_keys = {key.lower(): request.json[key] for key in request.json if key in expected_keys and request.json[key] not in ('', None)}
+
+	collection = database['users']		# collection[table] being used
+
+	print(received_keys)
+
+	## learn about Async/Await
+
+	try:
+		if(request.args.get('loggedin') and isinstance(user_id, str)):
+			# assign a new secret key if password or email is changed
+			if ('password' in received_keys or 'email' in received_keys):
+				received_keys['secret_key'] = secrets.token_hex()
+
+			query = {'_id': ObjectId(user_id)}
+			document = collection.find(query)
+
+			if document and len(document) == 1:
+				collection.insert_one(received_keys)
+				response = jsonify({
+					message: 'User updated successfully. ',
+					log_out: True,
+					give_access: True,
+					data: list(document)
+				}), 200
+			else:
+				print('Multiple Documents Found. Server Error. ')
+				response = jsonify({
+					message: 'Server Error. Multiple Documents Found. ',
+					give_access: True,
+					log_out: False,
+					data: []
+				}), 500
+	except Exception as e:
+		print('Suffered Internal Server Error: ', e)
+		response = jsonify({
+			message: 'Internal Server Error. ',
+			give_access: False,
+			log_out: False,
+			data: [],
+		}), 500
+	return response
+
+
+
 
 
 @app.route('/user/creds/delete/<int:id>')
@@ -117,14 +212,24 @@ def deleteCreds(id: int):
 	pass
 
 @app.errorhandler(404)
-def not_found(error=None):
+@app.errorhandler(405)
+def requestNotFound(error=None):
 	response = jsonify({
-		'status': 404,
+		'status': 405,
 		'message': 'Request not found for url: ' + request.url
 	})
-	response.status_code = 404
+	response.status_code = 405
 
 	return response
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ routes/__init__.py
@@ -0,0 +1,4 @@
+from authentication import Authentication
+
+if __name__ == '__main__':
+	authentication = Authentication()
